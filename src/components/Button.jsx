ea576a6d8705ce989d44882b6f744c0492ad8269
manideep <manideepgupta144@gmail.com>
2023-01-26 16:29:14 +0530

    [refact]: updated font-family to all components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
-
-
 const StyledButton = styled.button`
-    padding: 8px 20px;
-    background: ${({bgColor})=>bgColor};
-    border:none;
-    border-radius:4px;
-    color:#FFF;
-    font-family: 'Hanken Grotesk', sans-serif;
-    font-weight:500;
-    font-style:normal;
-    line-height:24px;
-    font-size:14px;
-    width:107px;
-    height:40px;
+  padding: 8px 20px;
+  background: ${({ bgColor }) => bgColor};
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+
+  width: 107px;
+  height: 40px;
 `;
 function Button({ bgColor, text, onClick }) {
   return (
