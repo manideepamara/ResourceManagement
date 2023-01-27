bc93eb44549a08d3057a65aa7695e438a6ae296a
manideep <manideepgupta144@gmail.com>
2023-01-27 19:57:36 +0530

    [feat]: add breadcrumb, rightImage; update arrowicon

--- src/components/ArrowIcon.jsx
+++ src/components/ArrowIcon.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
-function ArrowIcon({ left }) {
+function ArrowIcon({ left, formArrow }) {
+  if (formArrow) {
+    return (
+      <svg
+        width="7"
+        height="12"
+        viewBox="0 0 7 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M5.87352 1.34331e-08L7 1.15074L2.25296 6L7 10.8493L5.87352 12L0.684789 6.69953C0.304253 6.3108 0.304254 5.68919 0.684789 5.30046L5.87352 1.34331e-08Z"
+          fill="#171F46"
+        />
+      </svg>
+    );
+  }
   if (left) {
     return (
       <svg
---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+
+import ArrowIcon from "./ArrowIcon";
+
+
+const Wrapper = styled.div`
+    display:flex;
+    gap:9px;
+    font-size:12px;
+    line-height:16px;
+    font-weight:300;
+    color:#7E858E;
+    align-items:center;
+
+`;
+function BreadCrumb({ text }) {
+  return (
+    <Wrapper>
+      <ArrowIcon formArrow />
+      <span>{text}</span>
+    </Wrapper>
+  );
+}
+
+export default BreadCrumb;
