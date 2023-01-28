81181dfc68a94b6ff6f33b6957cd33a373216865
manideep <manideepgupta144@gmail.com>
2023-01-28 11:31:21 +0530

    [feat]: add form validations for createItem

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import styled from "styled-components";
 import Button from "./Button";
 
@@ -40,6 +43,15 @@ const DropDown = styled.select`
   background-image: url("data:image/svg+xml,%3Csvg width='12' height='7' viewBox='0 0 12 7' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M0 1.12648L1.15074 0L6 4.74704L10.8493 0L12 1.12648L6.69953 6.31521C6.3108 6.69575 5.68919 6.69575 5.30046 6.31521L0 1.12648Z' fill='%23171F46'/%3E%3C/svg%3E%0A");
 `;
 
+const TextArea = styled.textarea`
+  width: ${({ width }) => `${width || 304}px`};
+  height: ${({ height }) => `${height || 24}px`};
+  border: 1px solid #d7dfe9;
+  color: ${({ color }) => color || "#171F46"};
+  outline: none;
+  padding: 8px 0 8px 16px;
+`;
+
 const FormWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -51,52 +63,158 @@ const ItemWrapper = styled.div`
   gap: 8px;
   flex-direction: column;
 `;
+
+const ErrorMessage = styled.p`
+  color: red;
+`;
 function Form({ setTabList, tabList }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    icon_url: "",
+    link: "",
+    description: "",
+    category: "",
+    tag: tabList.selected,
+  });
+  const [error, setError] = useState({
+    title: false,
+    icon_url: false,
+    link: false,
+    description: false,
+    category: false,
+  });
+  const isValidFormData = () => {
+    let isValid = true;
+    const data = {
+      title: false,
+      icon_url: false,
+      link: false,
+      description: false,
+      category: false,
+    };
+    const expression =
+      /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+    const regex = new RegExp(expression);
+    if (formData.title.length == 0) {
+      isValid = false;
+      data.title = true;
+    }
+    if (!formData.link.match(regex)) {
+      isValid = false;
+      data.link = true;
+    }
+    if (!formData.icon_url.match(regex)) {
+      isValid = false;
+      data.icon_url = true;
+    }
+    if (formData.category.length === 0) {
+      isValid = false;
+      data.category = true;
+    }
+    if (formData.description.length === 0) {
+      isValid = false;
+      data.description = true;
+    }
+    return { isValid, data };
+  };
+  const onSubmit = () => {
+    const { isValid, data } = isValidFormData();
+    setError(data);
+    if (!isValid) {
+      toast.error("Failed at form validation", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        theme:"colored"
+      });
+      return;
+    }
+    fetch(
+      "https://media-content.ccbp.in/website/react-assignment/add_resource.json"
+    )
+      .then((res) => {
+        console.log("success");
+        return res.json();
+      })
+      .then((res) => {
+        console.log(res);
+        toast.success("Item created successfully", {
+          position: toast.POSITION.BOTTOM_CENTER,
+          theme:"colored"
+        });
+      })
+      .catch((e) => {
+        console.log(e, "failed");
+        toast.error("API Failure", {
+          position: toast.POSITION.BOTTOM_CENTER,
+          theme:"colored"
+        });
+      });
+  };
   return (
-    <FormWrapper>
-      <FormTitle>Item Details</FormTitle>
-      <ItemWrapper>
-        <Label>item title</Label>
-        <TextBox type="text" />
-      </ItemWrapper>
-      <ItemWrapper>
-        <Label>link</Label>
-        <TextBox type="text" color="#0B69FF" />
-      </ItemWrapper>
-      <ItemWrapper>
-        <Label>icon url</Label>
-        <TextBox type="text" color="#0B69FF" />
-      </ItemWrapper>
-      <ItemWrapper>
-        <Label>tag name</Label>
-        <DropDown
-          onChange={(e) => {
-            setTabList({ ...tabList, selected: parseInt(e.target.value) });
-          }}
-        >
-          {tabList.items.map((tab, idx) =>
-            idx === tabList.selected ? (
-              <option value={idx} selected key={idx}>
-                {tab}
-              </option>
-            ) : (
-              <option value={idx} key={idx}>
-                {tab}
-              </option>
-            )
+    <>
+      <FormWrapper
+        onChange={(e) => {
+          setFormData({ ...formData, [e.target.id]: e.target.value });
+        }}
+      >
+        <FormTitle>Item Details</FormTitle>
+        <ItemWrapper>
+          <Label>item title</Label>
+          <TextBox type="text" id="title" />
+          {error.title && (
+            <ErrorMessage>Title should not be empty</ErrorMessage>
           )}
-        </DropDown>
-      </ItemWrapper>
-      <ItemWrapper>
-        <Label>category</Label>
-        <TextBox type="text" />
-      </ItemWrapper>
-      <ItemWrapper>
-        <Label>description</Label>
-        <TextBox type="text" height={64} width={296} />
-      </ItemWrapper>
-      <Button bgColor={"#0B69FF"} text={"CREATE"} />
-    </FormWrapper>
+        </ItemWrapper>
+        <ItemWrapper>
+          <Label>link</Label>
+          <TextBox type="text" color="#0B69FF" id="link" />
+          {error.link && <ErrorMessage>Link Should be valid</ErrorMessage>}
+        </ItemWrapper>
+        <ItemWrapper>
+          <Label>icon url</Label>
+          <TextBox type="text" color="#0B69FF" id="icon_url" />
+          {error.icon_url && (
+            <ErrorMessage>icon path Should be valid</ErrorMessage>
+          )}
+        </ItemWrapper>
+        <ItemWrapper>
+          <Label>tag name</Label>
+          <DropDown
+            id="tag"
+            onChange={(e) => {
+              setTabList({ ...tabList, selected: parseInt(e.target.value) });
+            }}
+          >
+            {tabList.items.map((tab, idx) =>
+              idx === tabList.selected ? (
+                <option value={idx} defaultValue key={idx}>
+                  {tab}
+                </option>
+              ) : (
+                <option value={idx} key={idx}>
+                  {tab}
+                </option>
+              )
+            )}
+          </DropDown>
+        </ItemWrapper>
+        <ItemWrapper>
+          <Label>category</Label>
+          <TextBox type="text" id="category" />
+          {error.category && (
+            <ErrorMessage>category Should be mandatory</ErrorMessage>
+          )}
+        </ItemWrapper>
+        <ItemWrapper>
+          <Label>description</Label>
+          <TextArea type="text" height={64} width={296} id="description" />
+          {error.description && (
+            <ErrorMessage>Description Should be mandatory</ErrorMessage>
+          )}
+        </ItemWrapper>
+        <Button bgColor={"#0B69FF"} text={"CREATE"} onClick={onSubmit} />
+      </FormWrapper>
+      <ToastContainer />
+    </>
   );
 }
 
